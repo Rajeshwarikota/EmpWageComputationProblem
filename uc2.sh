aa0d5f590f059e36115bcdfedfad9a9269b82ea7
Rajeshwarikota <kotarajeswari003.com>
2022-12-28 19:57:39 +0530

    Added: uc9

--- uc2.sh
+++ uc2.sh
@@ -9,6 +9,8 @@ empRatePerHr=20;
 numWorkingDays=20;
 day=1;
 
+declare -A day
+
 function calculateWorkingHour() {
 	case $randomCheck in
              $FullTime)
@@ -37,6 +39,7 @@ randomCheck=$((RANDOM%3));
 	((day++));
 echo "Daily Wage:"${salary[@]}
 echo "Total Wage:"${totalSalary[@]}
+echo "Day: "${day[@]}
 done
 echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
 

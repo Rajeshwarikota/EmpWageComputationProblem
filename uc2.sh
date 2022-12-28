f9f404b808ed329e4ed340229e8b39d1a6fa1f65
Rajeshwarikota <kotarajeswari003.com>
2022-12-28 16:34:24 +0530

    Added: uc2

---
+++ uc2.sh
@@ -0,0 +1,17 @@
+echo "Welcome to Employee Wage Computation Program"
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+echo "Employee is present"
+empRatePerHr=20
+empHrs=8;
+salary=$(($empHrs * $empRatePerHr))
+echo "salary="$salary
+else
+echo "Employee is absent"
+echo "salary="$salary
+
+fi

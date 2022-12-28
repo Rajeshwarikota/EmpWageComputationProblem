1e241b724c4347bc287d3ced931ee9c910055a7b
Rajeshwarikota <kotarajeswari003.com>
2022-12-28 17:26:42 +0530

    Added: uc5

--- uc2.sh
+++ uc2.sh
@@ -1,22 +1,30 @@
-
+ 
 echo "Welcome to Employee Wage Computation Program"
 
 isPartTime=1;
 isFulltime=2;
-empRatePerHr=20
+totalSalary=0;
+empRatePerHr=20;
+numWorkingDays=20;
+
+for(( day=1;day<=numWorkingDays;day++))
+
+do
 randomCheck=$((RANDOM%3));
 
-case $randomCheck in 
-           $FullTime)
+case $randomCheck in $isFullTime)
             empHrs=8;;
 
-            $PartTime)
+            $isPartTime)
             empHrs=4;;
           *)
 
-            empHrs=0
-          ;;
+            empHrs=0;;
 esac
 
 salary=$(($empHrs * $empRatePerHr))
+totalSalary=$(($totalSalary+$salary))
+
+done
+echo "employee has earned $totalSalary$ in a month";
 echo "salary=$salary"
